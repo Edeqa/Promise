ff04a9a38f6dde6a00cca164bfd3c41f01fdcc7d
Edward <tujger@users.noreply.github.com>
2018-06-15 16:45:23 -0400

    Update Promise.js

--- Promise.js
+++ Promise.js
@@ -1,40 +1,40 @@
-function Promise(callback) {
+function Promise (callback) {
     this._pending = [];
     this._callback = callback;
     this.PENDING = "pending";
     this.RESOLVED = "resolved";
     this.REJECTED = "rejected";
     this.PromiseState = this.PENDING;
-    this._catch = function(error) {
+    this._catch = function (error) {
         console.error(error);
     };
-    setTimeout(function(){
+    setTimeout(function () {
         try {
             callback.call(this, this.resolve.bind(this), this.reject.bind(this));
-        } catch(error) {
+        } catch (error) {
             this.reject(error);
         }
     }.bind(this), 0)
-}
-Promise.prototype.resolve = function(object) {
-    if(this.PromiseState !== this.PENDING) return;
-    while(this._pending.length > 0) {
+};
+Promise.prototype.resolve = function (object) {
+    if (this.PromiseState !== this.PENDING) return;
+    while (this._pending.length > 0) {
         var callbacks = this._pending.shift();
         try {
             var resolve = callbacks.resolve;
-            if(resolve instanceof Promise) {
+            if (resolve instanceof Promise) {
                 resolve._pending = resolve._pending.concat(this._pending);
                 resolve._catch = this._catch;
                 resolve.resolve(object);
                 return resolve;
             }
-            object = resolve.call(this,object);
-            if(object instanceof Promise) {
+            object = resolve.call(this, object);
+            if (object instanceof Promise) {
                 object._pending = object._pending.concat(this._pending);
                 object._catch = this._catch;
                 return object;
             }
-        } catch(error) {
+        } catch (error) {
             (callbacks.reject || this._catch).call(this, error);
             return;
         }
@@ -42,89 +42,99 @@ Promise.prototype.resolve = function(object) {
     this.PromiseState = this.RESOLVED;
     return object;
 };
-Promise.prototype.reject = function(error) {
-    if(this.PromiseState !== this.PENDING) return;
+Promise.prototype.reject = function (error) {
+    if (this.PromiseState !== this.PENDING) return;
     this.PromiseState = this.REJECTED;
     try {
         this._catch(error);
-    } catch(e) {
+    } catch (e) {
         console.error(error, e);
     }
 };
-Promise.prototype.then = function(onFulfilled, onRejected) {
-    onFulfilled = onFulfilled || function(result) {
+Promise.prototype.then = function (onFulfilled, onRejected) {
+    onFulfilled = onFulfilled || function (result) {
         return result;
     };
     this._catch = onRejected || this._catch;
-    this._pending.push({resolve:onFulfilled, reject: onRejected});
+    this._pending.push({resolve: onFulfilled, reject: onRejected});
     return this;
 };
-Promise.prototype.catch = function(onRejected) {
-    var onFulfilled = function(result) {
+Promise.prototype.catch = function (onRejected) {
+    var onFulfilled = function (result) {
         return result;
     };
     this._catch = onRejected || this._catch;
-    this._pending.push({resolve:onFulfilled, reject: onRejected});
+    this._pending.push({resolve: onFulfilled, reject: onRejected});
     return this;
 };
-Promise.all = function(array) {
-    return new Promise(function() {
+Promise.all = function (array) {
+    return new Promise(function () {
         var self = this;
         var counter = 0;
         var finishResult = [];
-        function success(item) {
+
+        function success(item, index) {
             counter++;
-            finishResult.push(item);
-            if(counter >= array.length) {
+            finishResult[index] = item;
+            if (counter >= array.length) {
                 self.resolve(finishResult);
             }
         }
-        array.map(function(item) {
-            if(item instanceof Promise) {
-                item.then(function(result) {
-                    success(result);
-                }, function(error) {
-                    array.map(function(item) {item.PromiseState = Promise.REJECTED});
+        for(var i in array) {
+            var item = array[i];
+            if (item instanceof Promise) {
+                item.then(function (result) {
+                    success(result,this);
+                }.bind(i), function (error) {
+                    array.map(function (item) {
+                        item.PromiseState = Promise.REJECTED
+                    });
                     self._catch(error);
                 })
             } else {
-                success(item);
+                success(item, i);
             }
-        })
+        }
     });
 };
-Promise.race = function(array) {
-    return new Promise(function() {
+Promise.race = function (array) {
+    return new Promise(function () {
         var self = this;
         var counter = 0;
         var finishResult = [];
-        array.map(function(item) {
-            if(item instanceof Promise) {
-                item.then(function(result) {
-                    array.map(function(item) {item.PromiseState = Promise.REJECTED});
+        array.map(function (item) {
+            if (item instanceof Promise) {
+                item.then(function (result) {
+                    array.map(function (item) {
+                        item.PromiseState = Promise.REJECTED
+                    });
                     self.resolve(result);
-                }, function(error) {
-                    array.map(function(item) {item.PromiseState = Promise.REJECTED});
+                }, function (error) {
+                    array.map(function (item) {
+                        item.PromiseState = Promise.REJECTED
+                    });
                     self._catch(error);
                 })
             } else {
-                array.map(function(item) {item.PromiseState = Promise.REJECTED});
+                array.map(function (item) {
+                    item.PromiseState = Promise.REJECTED
+                });
                 self.resolve(item);
             }
         })
     });
 };
-Promise.resolve = function(value) {
-    return new Promise(function(resolve, reject) {
+Promise.resolve = function (value) {
+    return new Promise(function (resolve, reject) {
         try {
             resolve(value);
-        } catch(error) {
+        } catch (error) {
             reject(error);
         }
     });
 };
-Promise.reject = function(error) {
-    return new Promise(function(resolve, reject) {
+Promise.reject = function (error) {
+    return new Promise(function (resolve, reject) {
         reject(error);
     });
 }
